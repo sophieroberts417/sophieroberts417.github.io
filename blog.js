01d08bd752ab78b69d493094099d7b99bba164d5
sophieroberts417 <59858502+sophieroberts417@users.noreply.github.com>
2022-09-24 13:00:36 +0200

    miscellaneous
    fix images on int. page, add smooth scroll, hopefully fixed page links

--- blog.js
+++ blog.js
@@ -36,36 +36,27 @@ document.getElementById("body").onscroll = function myFunction() {
     document.documentElement.scrollTop = 0;
   }
   
-  // gallery functions below
-  let slideIndex = 1;
-  showSlides(slideIndex);
+  // blog post pop ups
+  const modal = document.querySelector("#modal");
+  const openModal = document.querySelector(".open-button");
+  const closeModal = document.querySelector(".close-button");
   
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
+  openModal.addEventListener("click", () => {
+    modal.showModal();
+  });
   
-  function currentSlide(n) {
-    showSlides((slideIndex = n));
-  }
+//   closeModal.addEventListener("click", () => {
+//     modal.close();
+//   });
   
-  function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-  }
-  
-  
+  // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// smooth scroll into view on load
+document.getElementById("navbar").scrollIntoView();
+window.SmoothScrollOptions = { animationTime: 1000 };
