13637823a610c9c4804ff53de5309b5f38e53cc3
sophieroberts417 <59858502+sophieroberts417@users.noreply.github.com>
2022-10-06 12:25:32 +0200

    fix scroll into view bug

--- pnt01.js
+++ pnt01.js
@@ -36,37 +36,6 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
-// gallery functions below
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
 
 // smooth scroll into view on load
 document.getElementById("navbar").scrollIntoView();
